f85c2682681a98399b63ab2db6b78a51c179ec9c
NineTailF0x <liams62@nycstudents.net>
2026-05-21 09:40:34 -0400

    Bottle

--- Question-2/Solution.java
+++ Question-2/Solution.java
@@ -1,1 +1,16 @@
+public class Bottle{
+    private double intial;
+    private double current;
+    public Bottle(double intial){
+        this.intial = intial;
+        this.current = intial;
 
+    }
+    public double updateAmount(double used){
+        current -= used;
+        if (current < (intial / 4)){
+            current = intial;
+        }
+        return current;
+    }
+}
