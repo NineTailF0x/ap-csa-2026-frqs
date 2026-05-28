f3c89d29c749792ee0dc79fa88badcb5ad162224
NineTailF0x <liams62@nycstudents.net>
2026-05-28 09:50:53 -0400

    Update Solution.java 3

--- Question-3/Solution.java
+++ Question-3/Solution.java
@@ -1,1 +1,12 @@
 
+public int moreHistoryThanMathAbcences(){
+    int count = 0;
+    for (int i = 0; i < historyList.size(); i++){
+        if(historyList[i].getStudentID().equals(mathList[i].getStudentID())){
+            if(historyList[i].getAbcences() > mathList[i].getAbcences()){
+                count ++;
+            }
+        }
+    }
+    return(count);
+}
