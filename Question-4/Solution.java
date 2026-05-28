76c17b6ebb771a6eaeb511087baa2194a9fdb063
NineTailF0x <liams62@nycstudents.net>
2026-05-28 10:00:21 -0400

    Update Solution.java 4

--- Question-4/Solution.java
+++ Question-4/Solution.java
@@ -1,1 +1,15 @@
-
+public in getPointForRow(int targetRow){
+  int total = 0;
+  boolean same = false;
+  for(int i = 0; i < board[targetRow].length();i++){
+    total += board[targetRow][i].getPoints;
+    if(!board[targetRow][i].getColor.equals(board[targetRow][0])){
+      same = false;
+    }
+  }
+  if(same == true){
+    return(total * 2)
+  } else {
+    return(total)
+  }
+}
